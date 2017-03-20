5df6a055a687aeaed4b62e24fd533287ec68b4b2
Omar Shibli <omarshib@gmail.com>
2017-03-19 22:04:09 -0400

    temp

--- src/components/dash/Dashboard.vue
+++ src/components/dash/Dashboard.vue
@@ -1,98 +1,121 @@
 <template>
-  <div id='app'>
+  <section class='content'>
+    <div id='app'>
 
-    <div class='form-group'>
-      <dropzone id='mainDropzone' url='/' v-on:vdropzone-file-added='fileAdded'></dropzone>
+      <div class='row form-group'>
+        <dropzone id='mainDropzone' url='/' v-on:vdropzone-file-added='fileAdded' v-on:vdropzone-removed-file='fileRemoved'>
+        </dropzone>
+      </div>
+
+      <div class='row form-group'>
+        <div class='input-group'>
+          <span class='input-group-addon'>$</span>
+          <input placeholder='public key' type='text' class='form-control' id='publicKey' name='publicKey' v-model='publicKey'>
+        </div>
+      </div>
+
+      <div class='row form-group'>
+        <div class='text-center'>
+            <button class='btn btn-primary btn-lg' v-on:click='generate($event)'>Generate</button>
+            <button class='btn btn-primary btn-lg' v-on:click='reset($event)'>Reset</button>
+        </div>
+      </div>
+
+      <div class='row text-center' v-if='resultReady'>
+        <div class='col-md-6 col-md-offset-3'>
+          <div class='info-box bg-aqua'>
+            <span class='info-box-icon'><i class='ion-ios-chatbubble-outline'></i></span>
+            <div class='info-box-content'>
+              <span class='info-box-text'>Payment Base</span>
+              <span class='info-box-number'>{{ paymentBase }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
-
-    <form class=''>
-      <input type='hidden' v-model='filePath'>
-
-      <div class='input-group form-group'>
-        <span class='input-group-addon'>$</span>
-        <input placeholder='public key' type='text' class='form-control' id='publicKey' name='publicKey' v-model='publicKey'>
-      </div>
-
-      <div class='text-center form-group'>
-        <button type='submit' class='btn btn-primary btn-lg'  v-on:click='generate($event)'>Generate</button>
-      </div>
-
-    </form>
-
-  </div>
+  </section>
 </template>
 
 <script>
-  import Dropzone from 'vue2-dropzone'
+import Dropzone from 'vue2-dropzone'
+import crypto from 'crypto'
 
-  Dropzone.props.autoProcessQueue = {
-    type: Boolean,
-    required: false,
-    default: function () {
-      return false
-    }
+Dropzone.props.autoProcessQueue = {
+  type: Boolean,
+  required: false,
+  default: function () {
+    return false
   }
+}
 
-  // function readFile(file) {
-  //   var fr = new FileReader();
-  //   fr.onload = (e) => {
-  //     // e.target.result should contain the text
-  //     console.log("===>", e)
-  //   }
-  //   fr.readAsText(file)
-  // }
-
-  Dropzone.props.useCustomDropzoneOptions = {
-    type: Boolean,
-    required: false,
-    default: function () {
-      return false
+export default {
+  name: 'MainApp',
+  components: {
+    Dropzone
+  },
+  data: function () {
+    return {
+      publicKey: '',
+      paymentBase: '',
+      resultReady: true,
+      fileSignatures: []
     }
-  }
-
-  Dropzone.props.dropzoneOptions = {
-    accept: function (file, done) {
-      if (file.name === 'justinbieber.jpg') {
-        done('Naha, you don\'t.')
-      } else {
-        done()
-      }
-    }
-  }
-
-  console.log(Dropzone.methods)
-
-  export default {
-    name: 'MainApp',
-    components: {
-      Dropzone
+  },
+  methods: {
+    reset: function (event) {
+      this.resultReady = false
+      this.publicKey = ''
+      this.fileSignatures = []
+      this.paymentBase = ''
+      //  TODO:: clear dropzone
     },
-    data () {
-      return {
-        publicKey: ''
-      }
+    generate: function (event) {
+      // now we have access to the native event
+      if (event) event.preventDefault()
+
+      console.log(this.publicKey)
+      console.log(this.fileSignatures)
+
+      this.paymentBase = 'foooo'
     },
-    methods: {
-      generate: function (event) {
-        // now we have access to the native event
-        if (event) event.preventDefault()
-        console.log(this.publicKey)
-        console.log(this.filePath)
-      },
-      fileAdded: function (event) {
-        console.log(event)
-        this.filePath = event
+    fileAdded: function (file) {
+      const that = this
+      that.fileSignatures[file.name] = {
+        status: 'initial'
       }
+      const fileReader = new window.FileReader()
+      const hash = crypto.createHash('sha512')
+      fileReader.onload = function (e) {
+        that.fileSignatures[file.name] = {
+          status: 'loaded'
+        }
+
+        hash.update(e.target.result, 'utf8')
+        const signature = hash.digest('hex')
+        that.fileSignatures[file.name] = {
+          status: 'digested',
+          signature: signature
+        }
+
+        console.log(that.fileSignatures)
+      }
+      // this.fileSignatures = event
+      fileReader.readAsText(file)
+    },
+    fileRemoved: function (file, error, xhr) {
+      delete this.fileSignatures[file.name]
     }
   }
+}
 
-  // Dropzone.options.mainDropzone.accept = function (file, done) {
-  //   console.log('fooooo', file)
-  //   done()
-  // }
+// Dropzone.options.mainDropzone.accept = function (file, done) {
+//   console.log('fooooo', file)
+//   done()
+// }
 </script>
 
 <style scoped>
-  @import url('~dropzone/dist/dropzone.css');
-  @import 'https://fonts.googleapis.com/css?family=Roboto';
+@import url('~dropzone/dist/dropzone.css');
+@import 'https://fonts.googleapis.com/css?family=Roboto';
 </style>
