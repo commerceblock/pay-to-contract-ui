03a9896cdf2a132dd632debf6f73e40b705efb26
Omar Shibli <omarshib@gmail.com>
2017-04-07 21:20:04 -0400

    added helpers file

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+// copied bravely from http://stackoverflow.com/a/8809472/1769302
+export function uuid () {
+  var d = new Date().getTime()
+
+  /* global performance */
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+    d += performance.now() // use high-precision timer if available
+  }
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0
+    d = Math.floor(d / 16)
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+  })
+}
