92434ffbb6fecf72e0a0055284605c59fed05c5a
Omar Shibli <omarshib@gmail.com>
2017-04-15 18:26:11 -0400

    fixed redeem-contract modal transition effects

--- src/components/redeem-contract/Modal.vue
+++ src/components/redeem-contract/Modal.vue
@@ -1,30 +1,37 @@
 <template>
 <transition name="modal">
-  <div class="invoice-modal-mask">
-    <div class="invoice-modal-wrapper modal">
-      <div class="modal-dialog">
-        <!-- Modal content-->
-        <div class="modal-content invoice-modal-container">
+  <div class="modal-mask">
+    <div class="modal-wrapper">
+      <div class="modal-container">
+
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" @click="close">&times;</button>
-            <h4 class="modal-title">Redeem Invoice</h4>
+            <slot name="header">
+              <button type="button" class="close" data-dismiss="modal" @click="close">&times;</button>
+              <h4 class="modal-title">Redeem Invoice</h4>
+            </slot>
           </div>
+
           <div class="modal-body">
-            <div class="form-group">
-              <label>Payment Address - Private Key</label>
-              <input class="form-control" readonly="readonly" type="text" v-model="paymentAddressPrivateKey" />
-              <div>
-                <qrcode :cls="'invoice-qr'" value="paymentAddressPrivateKey" />
+            <slot name="body">
+              <div class="form-group">
+                <label>Payment Address - Private Key</label>
+                <input class="form-control" readonly="readonly" type="text" v-model="paymentAddressPrivateKey" />
+                <div>
+                  <qrcode :cls="'qr-container'" value="paymentAddressPrivateKey" />
+                </div>
               </div>
-            </div>
+            </slot>
           </div>
+
           <div class="modal-footer">
-            <a :href="fileData" :download="fileName">
-              <button type="button" class="btn btn-primary">Export</button>
-            </a>
-            <button type="button" class="btn btn-primary" @click="close">Close</button>
+            <slot name="footer">
+              <a :href="fileData" :download="fileName">
+                <button type="button" class="btn btn-primary">Export</button>
+              </a>
+              <button type="button" class="btn btn-primary" @click="close">Close</button>
+            </slot>
           </div>
-        </div>
+
       </div>
     </div>
   </div>
@@ -66,6 +73,115 @@ export default {
     }
   }
 }
+</script>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  vertical-align: middle;
+  margin-top: 40px;
+}
+
+.modal-container {
+  width: 800px;
+  height: 400px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+.qr-container {
+  margin: 10px 10px 0px 0px
+}
+</style>
+
+
+<!--
+
+<template>
+<transition name="modal">
+  <div class="invoice-modal-mask">
+    <div class="invoice-modal-wrapper modal">
+      <div class="modal-dialog">
+        <div class="modal-content invoice-modal-container">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" @click="close">&times;</button>
+            <h4 class="modal-title">Redeem Invoice</h4>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label>Payment Address - Private Key</label>
+              <input class="form-control" readonly="readonly" type="text" v-model="paymentAddressPrivateKey" />
+              <div>
+                <qrcode :cls="'invoice-qr'" value="paymentAddressPrivateKey" />
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <a :href="fileData" :download="fileName">
+              <button type="button" class="btn btn-primary">Export</button>
+            </a>
+            <button type="button" class="btn btn-primary" @click="close">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</transition>
+</template>
+
+<script>
+
 </script>
 
 <style scoped>
@@ -136,3 +252,6 @@ export default {
   margin: 10px 10px 0px 0px
 }
 </style>
+
+
+-->
